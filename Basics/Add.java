805195b65fdcc26d10d2e60880506f28a67f99f0
Rishi Raj Singh <rishisahil1107@gmail.com>
2025-12-09 14:32:42 +0530

    Add program

---
+++ Basics/Add.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Add{
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a Number :");
+        int a = sc.nextInt();
+        System.out.println("Enter second Number :");
+        int b = sc.nextInt();
+        
+        System.out.println("Sum is :" + (a+b));
+        sc.close();
+    }
+}
